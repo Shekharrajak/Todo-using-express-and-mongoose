ccd9403d5c5942a767358e340f46ddafab5e8a7a
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-06-08 10:40:54 +0530

    added

--- todo/routes/index.js
+++ todo/routes/index.js
@@ -37,3 +37,13 @@ exports.destroy = function ( req, res ){
     });
   });
 };
+
+exports.edit = function ( req, res ){
+  Todo.find( function ( err, todos ){
+    res.render( 'edit', {
+        title   : 'Express Todo Example',
+        todos   : todos,
+        current : req.params.id
+    });
+  });
+};
