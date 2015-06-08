364a5903f5d5fe453e10f1a65e600b1ffdf92732
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-06-08 10:38:37 +0530

    added

--- todo/app.js
+++ todo/app.js
@@ -28,7 +28,8 @@ if ( 'development' == app.get( 'env' )) {
  
 app.get( '/', routes.index );
 app.post( '/create', routes.create );
- 
+ // add this line to the routes section
+app.get( '/destroy/:id', routes.destroy );
 http.createServer( app ).listen( app.get( 'port' ), function(){
   console.log( 'Express server listening on port ' + app.get( 'port' ));
 } );
--- todo/routes/index.js
+++ todo/routes/index.js
@@ -18,3 +18,22 @@ exports.create = function ( req, res ){
     res.redirect( '/' );
   });
 };
+
+// query db for all todo items
+exports.index = function ( req, res ){
+  Todo.find( function ( err, todos, count ){
+    res.render( 'index', {
+      title : 'Express Todo Example',
+      todos : todos
+    });
+  });
+};
+
+// remove todo item by its id
+exports.destroy = function ( req, res ){
+  Todo.findById( req.params.id, function ( err, todo ){
+    todo.remove( function ( err, todo ){
+      res.redirect( '/' );
+    });
+  });
+};
