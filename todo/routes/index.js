e5d72edcff04c03c9f24e4822aa7da5563c6df39
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-06-08 10:36:16 +0530

    added

--- todo/routes/index.js
+++ todo/routes/index.js
@@ -6,3 +6,15 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express todo example.' });
 };
+
+var mongoose = require( 'mongoose' );
+var Todo     = mongoose.model( 'Todo' );
+
+exports.create = function ( req, res ){
+  new Todo({
+    content    : req.body.content,
+    updated_at : Date.now()
+  }).save( function( err, todo, count ){
+    res.redirect( '/' );
+  });
+};
